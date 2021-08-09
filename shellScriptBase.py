fb28a67f83b7f701672ebc232830896224c2a3cd
Allen Stetson <allen.stetson@gmail.com>
2021-08-09 16:04:23 -0700

    docstring and pylint

--- shellScriptBase.py
+++ shellScriptBase.py
@@ -1,76 +1,107 @@
-#! /usr/bin/env python
+###############################################################################
+# Copyright (c) 2019 Allen Stetson, allen.stetson@gmail.com
+# All rights reserved. No duplication allowed.
+#
+# This file is part of nfdCallsheet.
+#
+# This software may not be copied and/or distributed without the express
+# permission of Allen Stetson.
+###############################################################################
+"""
+shellscript_base.py - A simple tool framework for creating shell scripts.
+
+This is technically not part of this tool, but since it lived outside of this
+package, and was leveraged by this code, I'm rolling it into the same github
+repo.
+
+"""
 import argparse
 
 class BaseShellScript(object):
+    """A basic shell script tool framework which provides some args.
+
+    Using argparse, this framework provides toggleable verbose printing,
+    debug level printing, and a default parser to which subclasses can
+    add their own commandline arguments.
+
+    """
     def __init__(self):
-        '''
+        """
         Initialization which sets up variable and calls argument registrations
-        '''
+        """
         self.args = None
         self.parser = None
         self.verbose = False
         self.debug = 0
         self._addDefaultArgs()
         self.registerArgs()
+        self._processArgs()
 
     def _addDefaultArgs(self):
-        '''
+        """
         Adds arguments which all shell scripts will have.
-        '''
-        self.parser = argparse.ArgumentParser(description='Base class for shell script <change this description>')
-        self.parser.add_argument('-v', '--verbose',
-                        	action='store_true',
-                        	help='verbose output',
-                        	)
-        self.parser.add_argument('-d', '--debug',
-                        	nargs='?',
-                        	const='0',
-                        	default='0',
-                        	help='debug output level',
-                        	)
+
+        """
+        self.parser = argparse.ArgumentParser(
+            description='Base class for shell script <change this description>'
+            )
+        self.parser.add_argument(
+            '-v',
+            '--verbose',
+            action='store_true',
+            help='verbose output',
+            )
+        self.parser.add_argument(
+            '-d', '--debug',
+            nargs='?',
+            const='0',
+            default='0',
+            help='debug output level',
+            )
 
     def registerArgs(self):
-        '''
-        This is here as a placeholder for children to add their own arguments to self.parser
-        '''
+        """
+        Placeholder for subclass to add their own arguments to self.parser.
+
+        Does not need to be implemented if there are no additional args, so
+        NotImplemented is not raised here.
+
+        """
         pass
 
     def _processArgs(self):
-        '''
-        Processes the contents of self.parser, assigns some class variables
-        '''
+        """
+        Processes the contents of self.parser, assigns some class variables.
+
+        """
         self.args = self.parser.parse_args()
         self.verbose = self.args.verbose
         self.debug = self.args.debug
 
     def run(self):
-        '''
-        Meat and potatoes, where the actual script happens
-        '''
-        self._processArgs()
-        self.printTestOutput()  # Fun test
+        """
+        Meat and potatoes, where the actual script happens. Required.
+        """
+        raise NotImplementedError
 
-    def printv(self, string):
-        '''
-        Prints string provided, as long as verbose is True
-        '''
-        if self.verbose: print(string)
+    def printv(self, printString):
+        """
+        Prints string provided, as long as verbose is True.
 
-    def printDebug(self, level, string):
-        '''
-        Prints string provided, as long as the debug level is greater than or equal to the number specified
-        '''
-        if int(self.debug) >= level: print(string)
+        Args:
+            printString (str): The verbose string to print.
 
-    def printTestOutput(self):
-        '''
-        Fun test output.
-        '''
-        print(self.args)
-        self.printv("I like toast.")
-        self.printDebug(3, "Toast is made of bread.")
-        self.printDebug(5, "Bread is made of wheat that has been processed.")
+        """
+        if self.verbose:
+            print(printString)
 
-if __name__ == "__main__":
-    thing = BaseShellScript()
-    thing.run()
+    def printDebug(self, level, printString):
+        """
+        Prints string provided if debug level is >= level number specified.
+
+        Args:
+            printString (str): The string to print if debug level is met.
+
+        """
+        if int(self.debug) >= level:
+            print(printString)
